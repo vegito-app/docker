996e0506aace6904aecc200d00858b3ea2dcfcac
David Berichon <david.berichon@vegito.app>
2026-06-13 15:47:27 +0200

    build(makefile): include local.mk Added `-include local.mk` in Makefile for conditional inclusion of local build targets, facilitating integration with the Vegito local project. Removed the unused target "docker-debian-trixie-golang-ci" from `debian.docker-bake.hcl` to clean up the build configuration. Introduced `local.mk` where helper targets update Docker image DAG parts essential for local project enhancements. These changes streamline build processes and centralize local-specific configurations.

---
+++ local.mk
@@ -0,0 +1,21 @@
+# Targets in this file are helper releasing 
+# for github.com/vegito-app/local integration.
+# It will update the DAG part used by this secondary project.
+
+vegito-local-update: \
+vegito-docker-hub-images-update \
+vegito-local-debian-images-update \
+vegito-local-debian-bookworm-images-update
+.PHONY: vegito-local-update
+
+vegito-local-debian-images-update: \
+vegito-docker-trixie-debian-flutter-desktop-x-images-update \
+vegito-docker-trixie-debian-robotframework-images-update \
+vegito-docker-trixie-debian-project-vscode-golang-ai-docker-images-update
+.PHONY: vegito-debian-update
+
+vegito-local-debian-bookworm-images-update: \
+vegito-docker-debian-flutter-desktop-x-images-update \
+vegito-docker-debian-robotframework-images-update \
+vegito-docker-debian-project-vscode-golang-ai-docker-images-update
+.PHONY: vegito-bookworm-debian-update
