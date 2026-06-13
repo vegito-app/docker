4b4fa176250dd2a65297d81e79f1ec3a27d39c4d
David Berichon <david.berichon@vegito.app>
2026-06-13 07:28:46 +0200

    feat(debian): add container entrypoint and install scripts
    Introduce new entrypoint and installation scripts to the Node.js Dockerfile in order to manage persistent configurations and caches. The `container-entrypoint.sh` initializes Debian, checks if nodejs-container-install should be executed based on the environment variable NODEJS_CONTAINER_INSTALL, and persists NPM configuration. This enhances the development environment by maintaining settings across container rebuilds, making it particularly useful for local development setups.

---
+++ debian/nodejs/container-entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# 🐧 Setup Debian
+debian-entrypoint.sh echo "✅ Debian setup complete."
+
+# Project container installation script to setup persistent configurations and caches.
+# If NODEJS_CONTAINER_INSTALL is set to "true", run the local-container-install.sh script
+# to install the necessary configurations and caches.
+# This is useful for local development environments where you want to keep your settings across container rebuilds.
+# You can set this variable in your devcontainer.json or in your environment before starting the container.
+# Example: export NODEJS_CONTAINER_INSTALL=true
+if [ "${NODEJS_CONTAINER_INSTALL:-true}" = true ]; then
+    nodejs-container-install.sh
+fi
+
+
+# Run the command
+exec "$@"
---
+++ debian/nodejs/container-install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -uo pipefail
+trap "echo Exited with code $?." EXIT
+
+# Use a local directory to persist container caches and configurations across container rebuilds.
+# You can override the default location by setting the LOCAL_DEV_CONTAINER_CACHE environment variable.
+# Example: export LOCAL_DEV_CONTAINER_CACHE=/path/to/your/local/cache
+# If LOCAL_DEV_CONTAINER_CACHE is not set, it will default to $LOCAL_WORKSPACE/.containers
+local_container_cache=${LOCAL_DEV_CONTAINER_CACHE:-${LOCAL_WORKSPACE:-${PWD}}/.containers/dev}
+mkdir -p $local_container_cache
+
+# NPM persistence
+# This allows you to persist your npm configuration across container rebuilds.
+NPM_DIR=${HOME}/.npm
+[ -d $NPM_DIR ] && mv $NPM_DIR ${NPM_DIR}_back
+mkdir -p ${local_container_cache}/npm
+ln -sf ${local_container_cache}/npm $NPM_DIR
